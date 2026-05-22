427d307eb5731ac98aa3118b8d94cdfd732d3403
push-shihab <shihabfamily539@gmail.com>
2026-05-22 22:28:37 +0600

    failed the deployment again

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -5,33 +5,32 @@ import React from "react";
 
 const NavLinks = ({ session }) => {
   const path = usePathname();
-
   return (
     <>
       <li
-        className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path == "/" && "border border-[#D97757] px-3 py-1 rounded-4xl"} transition-all duration-200 ease-in-out`}
+        className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path === "/" && "border border-[#D97757] px-3 py-1 rounded-4xl"} transition-all duration-200 ease-in-out`}
       >
         <Link href={"/"}>Home</Link>
       </li>
       <li
-        className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path == "/rooms" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
+        className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path === "/rooms" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
       >
         <Link href={"/rooms"}>Rooms</Link>
       </li>
       {session && (
         <>
           <li
-            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path == "/rooms/new" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
+            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path === "/rooms/new" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
           >
             <Link href={"/rooms/new"}>Add Room</Link>
           </li>
           <li
-            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path == "/rooms/self" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
+            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path === "/rooms/self" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
           >
             <Link href={"/rooms/self"}>My Rooms</Link>
           </li>
           <li
-            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path == "/rooms/bookings" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
+            className={`hover:text-[#D97757] rounded-4xl px-3 py-1 ${path === "/rooms/bookings" && "border border-[#D97757] px-3 py-1 rounded-4xl"}`}
           >
             <Link href={"/rooms/bookings"}>My Bookings</Link>
           </li>
